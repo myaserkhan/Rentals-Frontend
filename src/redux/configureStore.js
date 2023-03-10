741f726e31f3c804e35ddf36cd28d6a8d2e6af7d
John <kiokoj81@gmail.com>
2023-03-10 13:03:11 +0300

    Fix linter errors

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import carsReducer from './cars/carsSlice';
-import reservationsReducer from './reservations/reservationsSlice';
+// import carsReducer from './cars/carsSlice';
+// import reservationsReducer from './reservations/reservationsSlice';
 
 const reducer = combineReducers({
-  cars: carsReducer,
-  reservations: reservationsReducer,
+//   cars: carsReducer,
+//   reservations: reservationsReducer,
   // additional reducers could be added here
 });
 
