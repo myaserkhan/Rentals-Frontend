d0c07d27ddf655f0925163607370137caf62a50b
Yasir Khan <khanyaser007@gmail.com>
2023-03-13 17:49:04 +0500

    Change Home page design

--- src/App.js
+++ src/App.js
@@ -1,44 +1,40 @@
-// import React, { useEffect } from 'react';
-import React from 'react';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from 'react-router-dom';
+// import store from './redux/configureStore';
 import styled from 'styled-components';
 import DetailsPage from './pages/DetailsPage';
 // import { getCars } from './redux/cars/carsSlice';
 // import { getCities, getReservations } from './redux/reservations/reservationsSlice';
-// import Announcement from './components/Announcement';
+import Announcement from './components/Announcement';
 import Sidebar from './components/Sidebar';
 import HomePage from './pages/HomePage';
-import AddCar from './pages/AddCar';
-import './App.css';
+import LifeStyle from './pages/LifeStyle';
+import CarsHome from './components/cars/CarsHome';
 
 const Container = styled.div`
   display: flex;
-  max-width: 100vw;
-`;
-
-const App = () =>
-  // useEffect(() => {
-  //   store.dispatch(getCars());
-  //   store.dispatch(getCities());
-  //   store.dispatch(getReservations());
-  // }, []);
-  // eslint-disable-next-line implicit-arrow-linebreak
-  (
-    <Router>
-      {/* <Announcement /> */}
-      <Container>
-        <Sidebar />
-        <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          <Route path="/cars/:car_id/details" element={<DetailsPage />} />
-          <Route path="/cars/new" element={<AddCar />} />
-        </Routes>
-      </Container>
-    </Router>
-  );
-
+  `;
+// useEffect(() => {
+//   store.dispatch(getCars());
+//   store.dispatch(getCities());
+//   store.dispatch(getReservations());
+// }, []);
+// eslint-disable-next-line implicit-arrow-linebreak
+const App = () => (
+  <Router>
+    <Announcement />
+    <Container>
+      <Sidebar />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/cars" element={<CarsHome />} />
+        <Route path="/:car_id/details" element={<DetailsPage />} />
+        <Route path="/lifestyle" element={<LifeStyle />} />
+      </Routes>
+    </Container>
+  </Router>
+);
 export default App;
