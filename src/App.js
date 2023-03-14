7e72f02c8ea9c63707f97fbe78698314b3b73fd9
Yasir Khan <khanyaser007@gmail.com>
2023-03-14 18:24:06 +0500

    Seperate navigation routes

--- src/App.js
+++ src/App.js
@@ -4,54 +4,39 @@ import {
   Route,
 } from 'react-router-dom';
 import styled from 'styled-components';
-import DetailsPage from './pages/DetailsPage';
 // import { getCars } from './redux/cars/carsSlice';
 // import { getCities, getReservations } from './redux/reservations/reservationsSlice';
-// import Announcement from './components/Announcement';
-import Sidebar from './components/navigation/Sidebar';
 import Announcement from './components/Announcement';
+import Sidebar from './components/Sidebar';
 import HomePage from './pages/HomePage';
-import AddCar from './pages/AddCar';
-import LifeStyle from './pages/LifeStyle';
-import CarsHome from './components/cars/CarsHome';
-import MyReservations from './pages/MyReservations';
+import links from './navigation';
+
 import './App.css';
 
-const PageContainer = styled.div`
+const Container = styled.div`
   display: flex;
-  justify-content: center;
   max-width: 100vw;
 `;
 
-const AppContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  position: relative;
-`;
-
 const App = () =>
-  // useEffect(() => {
-  //   store.dispatch(getCars());
-  //   store.dispatch(getCities());
-  //   store.dispatch(getReservations());
-  // }, []);
-  // eslint-disable-next-line implicit-arrow-linebreak
+// useEffect(() => {
+//   store.dispatch(getCars());
+//   store.dispatch(getCities());
+//   store.dispatch(getReservations());
+// }, []);
+// eslint-disable-next-line implicit-arrow-linebreak
   (
     <Router>
-      <AppContainer>
-        <Announcement />
-        <PageContainer>
-          <Sidebar />
-          <Routes>
-            <Route exact path="/" element={<HomePage />} />
-            <Route path="/cars/:car_id/details" element={<DetailsPage />} />
-            <Route path="/cars/new" element={<AddCar />} />
-            <Route path="/cars" element={<CarsHome />} />
-            <Route path="/myreservations" element={<MyReservations />} />
-            <Route path="/lifestyle" element={<LifeStyle />} />
-          </Routes>
-        </PageContainer>
-      </AppContainer>
+      <Announcement />
+      <Container>
+        <Sidebar />
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          {links.map((link) => (
+            <Route key={link.id} path={link.path} element={link.element} />
+          ))}
+        </Routes>
+      </Container>
     </Router>
   );
 
